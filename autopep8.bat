590c9dcd16575f1489fe75c08b08f538d4be46fb
sepandhaghighi <sepand.haghighi@yahoo.com>
2023-01-08 20:24:11 +0330

    fix : autopep8 scripts added

---
+++ autopep8.bat
@@ -0,0 +1,3 @@
+python -m autopep8 pymilo --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose --ignore=E721
+python -m autopep8 otherfiles --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose --ignore=E721
+python -m autopep8 setup.py --recursive --aggressive --aggressive --in-place --pep8-passes 2000 --verbose
