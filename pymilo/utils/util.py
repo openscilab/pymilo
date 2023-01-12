c5c1983e850a083f603eef3acda3395c4ed1faf3
Sadra Sabouri <sabouri.sadra@gmail.com>
2023-01-12 12:14:43 +0330

    [:monocle_face:] add : docstring added to `utils`.

--- pymilo/utils/util.py
+++ pymilo/utils/util.py
@@ -2,15 +2,36 @@ from numpy import ndarray
 
 
 def get_sklearn_type(model):
+    """
+    Return sklearn model type.
+
+    :param model: sklearn model
+    :type model: any sklearn's model class
+    :return: model type as str
+    """
     raw_type = type(model)
     return str(raw_type).split(".")[-1][:-2]
 
 
 def is_primitive(obj):
+    """
+    Check if the given object is primitive.
+
+    :param obj: given object
+    :type obj: any valid type
+    :return: True if object is primitive
+    """
     return not hasattr(obj, '__dict__')
 
 
 def is_iterable(obj):
+    """
+    Check if the given object is iterable.
+
+    :param obj: given object
+    :type obj: any valid type
+    :return: True if object is iterable
+    """
     try:
         iter(obj)
         return True
@@ -22,7 +43,7 @@ def check_str_in_iterable(field, content):
     if (not (is_iterable(content))):
         return False
     if (isinstance(content, ndarray)):
-        # https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur#:~:text=There%20is%20a,adopt%20Numpy%20style.
+        # https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur.
         return False
     else:
         return field in content
