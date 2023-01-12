e8c7829104a54e1c64b03c47fb095a93ec34ffc0
Sadra Sabouri <sabouri.sadra@gmail.com>
2023-01-12 11:47:24 +0330

    edit : minor edits in `compare_model_outputs`.

--- pymilo/pymilo_func.py
+++ pymilo/pymilo_func.py
@@ -18,16 +18,15 @@ def to_sklearn_model(import_obj):
     return transport_linear_model(import_obj, Command.DESERIALZIE)
 
 
-def compare_model_outputs(exported_model_output,
-                          imported_model_output,
+def compare_model_outputs(exported_output,
+                          imported_output,
                           epsilon_error=10**(-8)):
-    if len(exported_model_output.keys()) != len(imported_model_output.keys()):
-        return False  # Todo throw exception
-    totalError = 0
-    for key in exported_model_output.keys():
-        if (not (key in imported_model_output.keys())):
-            return False  # Todo throw exception
-        totalError += np.abs(imported_model_output[key]) - \
-            np.abs(exported_model_output[key])
-    # print(f'totalError: {totalError}')
-    return np.abs(totalError) < epsilon_error
+    if len(exported_output.keys()) != len(imported_output.keys()):
+        return False  # TODO: throw exception
+    total_error = 0
+    for key in exported_output.keys():
+        if (not (key in imported_output.keys())):
+            return False  # TODO: throw exception
+        total_error += np.abs(imported_output[key]) - \
+            np.abs(exported_output[key])
+    return np.abs(total_error) < epsilon_error
