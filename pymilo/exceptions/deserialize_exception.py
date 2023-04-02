9514db711a5a665156a75cca522ebf8b1a155823
AHReccese <amirhssin6rst@gmail.com>
2023-04-02 20:21:10 +0430

    apply requested changes: using str.format

--- pymilo/exceptions/deserialize_exception.py
+++ pymilo/exceptions/deserialize_exception.py
@@ -9,14 +9,18 @@ class DeSerilaizatoinErrorTypes(Enum):
 class PymiloDeserializationException(PymiloException):
     def __init__(self, meta_data):            
         # Call the base class constructor with the parameters it needs
-        message = "Pymilo Deserialization failed since "
+        message = "Pymilo Deserialization failed since {reason}"
         error_type = meta_data['error_type']
-        if error_type == DeSerilaizatoinErrorTypes.CORRUPTED_JSON_FILE:
-            message += 'the given json file is not a valid .json file.'
-        elif error_type == DeSerilaizatoinErrorTypes.INVALID_MODEL:
-            message += 'the given model is not supported or is not a valid model.'
-        elif error_type == DeSerilaizatoinErrorTypes.VALID_MODEL_INVALID_INTERNAL_STRUCTURE:
-            message += 'the given model has some non-standard customized internal objects or functions.'
+        error_type_to_message = {
+            DeSerilaizatoinErrorTypes.CORRUPTED_JSON_FILE: 'the given json file is not a valid .json file.',
+            DeSerilaizatoinErrorTypes.INVALID_MODEL: 'the given model is not supported or is not a valid model.',
+            DeSerilaizatoinErrorTypes.VALID_MODEL_INVALID_INTERNAL_STRUCTURE: 'the given model has some non-standard customized internal objects or functions.'
+        }
+        if error_type in error_type_to_message.keys():
+            reason = error_type_to_message[error_type]
+        else:
+            reason = "an Unknown error occurred."
+        message.format(reason = reason)
         super().__init__(message, meta_data)
 
     def to_pymilo_log(self):
@@ -28,14 +32,15 @@ class PymiloDeserializationException(PymiloException):
     def to_pymilo_issue(self):
         pymilo_report = self.to_pymilo_log()
         help_request = "\n\nIn order to help us enhance Pymilo's functionality, please open an issue associated with this error and put the message below inside.\n"
-        discription = "#### Description\n" + "Pymilo Import failed."
-        steps_to_produce = "#### Steps/Code to Reproduce\n" + "It is auto-reported from the pymilo logger."
-        expected_behaviour = "#### Expected Behavior\n" + "A successfull Pymilo Import."
-        actual_behaviour = "#### Actual Behavior\n" + "Pymilo Import failed."
-        operating_system = "#### Operating System\n" + pymilo_report['os']['full-description']
-        python_version = "#### Python Version\n" + pymilo_report['versions']["python-version"]
-        pymilo_version = "#### PyMilo Version\n" + pymilo_report['versions']["pymilo-version"]
-        gathered_data = "#### Logged Data\n" + str(pymilo_report)
+        discription = "#### Description\n Pymilo Import failed."
+        steps_to_produce = "#### Steps/Code to Reproduce\n It is auto-reported from the pymilo logger."
+        expected_behaviour = "#### Expected Behavior\n A successfull Pymilo Import."
+        actual_behaviour = "#### Actual Behavior\n Pymilo Import failed."
+        operating_system = "#### Operating System\n {os}".format(os = pymilo_report['os']['full-description'])
+        python_version = "#### Python Version\n {python_version}".format(python_version = pymilo_report['versions']["python-version"])
+        pymilo_version = "#### PyMilo Version\n {pymilo_version}".format(pymilo_version = pymilo_report['versions']["pymilo-version"])
+        gathered_data = "#### Logged Data\n {logged_data}".format(str(logged_data = pymilo_report))
 
-        full_issue_form = help_request + discription + steps_to_produce + expected_behaviour + actual_behaviour + operating_system + python_version + pymilo_version + gathered_data
+        full_issue_form = help_request + discription + steps_to_produce + expected_behaviour + actual_behaviour \
+        + operating_system + python_version + pymilo_version + gathered_data
         return full_issue_form
--- pymilo/exceptions/serialize_exception.py
+++ pymilo/exceptions/serialize_exception.py
@@ -10,10 +10,14 @@ class PymiloSerializationException(PymiloException):
         # Call the base class constructor with the parameters it needs
         message = "Pymilo Serialization failed since "
         error_type = meta_data['error_type']
-        if error_type == SerilaizatoinErrorTypes.INVALID_MODEL:
-            message += 'the given model is not supported or is not a valid model.'
-        elif error_type == SerilaizatoinErrorTypes.VALID_MODEL_INVALID_INTERNAL_STRUCTURE:
-            message += 'the given model has some non-standard customized internal objects or functions.'
+        error_type_to_message = {
+            SerilaizatoinErrorTypes.INVALID_MODEL: 'the given model is not supported or is not a valid model.',
+            SerilaizatoinErrorTypes.VALID_MODEL_INVALID_INTERNAL_STRUCTURE: 'the given model has some non-standard customized internal objects or functions.'
+            }
+        if error_type in error_type_to_message.keys():
+            message += error_type_to_message[error_type]
+        else:
+            message += "an Unknown error occurred."
         super().__init__(message, meta_data)
 
     def to_pymilo_log(self):
@@ -24,14 +28,15 @@ class PymiloSerializationException(PymiloException):
     def to_pymilo_issue(self):
         pymilo_report = self.to_pymilo_log()
         help_request = "\n\nIn order to help us enhance Pymilo's functionality, please open an issue associated with this error and put the message below inside.\n"
-        discription = "#### Description\n" + "Pymilo Export failed."
-        steps_to_produce = "\n#### Steps/Code to Reproduce\n" + "It is auto-reported from the pymilo logger."
-        expected_behaviour = "\n#### Expected Behavior\n" + "A successfull Pymilo Export."
-        actual_behaviour = "\n#### Actual Behavior\n" + "Pymilo Export failed."
-        operating_system = "\n#### Operating System\n" + pymilo_report['os']['full-description']
-        python_version = "\n#### Python Version\n" + pymilo_report['versions']["python-version"]
-        pymilo_version = "\n#### PyMilo Version\n" + pymilo_report['versions']["pymilo-version"]
-        gathered_data = "\n#### Logged Data\n" + str(pymilo_report)
-
-        full_issue_form = help_request + discription + steps_to_produce + expected_behaviour + actual_behaviour + operating_system + python_version + pymilo_version + gathered_data
+        discription = "#### Description\n Pymilo Export failed."
+        steps_to_produce = "\n#### Steps/Code to Reproduce\n It is auto-reported from the pymilo logger."
+        expected_behaviour = "\n#### Expected Behavior\n A successfull Pymilo Export."
+        actual_behaviour = "\n#### Actual Behavior\n Pymilo Export failed."
+        operating_system = "#### Operating System\n {os}".format(os = pymilo_report['os']['full-description'])
+        python_version = "#### Python Version\n {python_version}".format(python_version = pymilo_report['versions']["python-version"])
+        pymilo_version = "#### PyMilo Version\n {pymilo_version}".format(pymilo_version = pymilo_report['versions']["pymilo-version"])
+        gathered_data = "#### Logged Data\n {logged_data}".format(str(logged_data = pymilo_report))
+        
+        full_issue_form = help_request + discription + steps_to_produce + expected_behaviour + actual_behaviour \
+            + operating_system + python_version + pymilo_version + gathered_data
         return full_issue_form
