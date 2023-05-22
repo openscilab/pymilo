1bceb5a4109bfc56f898a969de04197bafdf9a6c
AHReccese <amirhssin6rst@gmail.com>
2023-05-22 09:16:43 +0330

    labelbinarizer_transporter.py's docstring added.

--- pymilo/transporters/labelbinarizer_transporter.py
+++ pymilo/transporters/labelbinarizer_transporter.py
@@ -11,11 +11,20 @@ class LabelBinarizerTransporter(AbstractTransporter):
 
     # SERIALIZATION
     def serialize(self, data, key, model_type):
+        """
+        serializes the LabelBinarizer field(if there is)
+        """
         if isinstance(data[key], preprocessing.LabelBinarizer):
             data[key] = self.get_serialized_label_binarizer(data[key])
         return data[key]
 
     def get_serialized_label_binarizer(self, label_binarizer):
+        """
+        serializes a LabelBinarizer object
+        :param label_binarizer: a label_binarizer object
+        :type label_binarizer: sklearn.preprocessing.LabelBinarizer
+        :return: pymilo serialized output of label_binarizer object
+        """
         data = label_binarizer.__dict__
         for key in data.keys():
             if isinstance(data[key], np.ndarray):
@@ -24,12 +33,22 @@ class LabelBinarizerTransporter(AbstractTransporter):
 
     # DESERIALIZATION
     def deserialize(self, data, key, model_type):
+        """
+        deserializes the LabelBinarizer field(if there is)
+        """
         content = data[key]
         if key != "_label_binarizer":
             return content
         return self.get_deserialized_label_binarizer(content)
 
     def get_deserialized_label_binarizer(self, content):
+        """
+        deserializes the pymilo serialized labelBinarizer field of the associated ML model
+        :param content: a label_binarizer object
+        :type content: sklearn.preprocessing.LabelBinarizer
+        :return: a sklearn.preprocessing.LabelBinarizer instance derived from the 
+        pymilo deserialized output of the previously pymilo serialized label_binarizer field.
+        """
         raw_lb = KEYS_NEED_PREPROCESSING_BEFORE_DESERIALIZATION["_label_binarizer"](
         )
         for key in content.keys():
