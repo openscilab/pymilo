1fc0b564ece6d838fe52ddbff189f1f6008fd4d6
AHReccese <amirhssin6rst@gmail.com>
2023-07-16 18:00:02 +0330

    autopep8 run for adam optimizer

--- pymilo/transporters/adamoptimizer_transporter.py
+++ pymilo/transporters/adamoptimizer_transporter.py
@@ -6,14 +6,14 @@ from .transporter import AbstractTransporter
 
 # Handling sklearn.neural_network._stochastic_optimizers.AdamOptimizer
 
+
 class AdamOptimizerTransporter(AbstractTransporter):
     """Customized PyMilo Transporter developed to handle AdamOptimizer field."""
 
     def serialize(self, data, key, model_type):
 
-        if (
-            (model_type == "MLPRegressor" and key == "_optimizer" and isinstance(data[key], AdamOptimizer)) 
-        ):
+        if ((model_type == "MLPRegressor" and key ==
+             "_optimizer" and isinstance(data[key], AdamOptimizer))):
             optimizer = data[key]
             data[key] = {
                 'params': {
@@ -29,12 +29,15 @@ class AdamOptimizerTransporter(AbstractTransporter):
 
         content = data[key]
         # if model_type != "MLPRegressor":
-        #    return content 
+        #    return content
         if is_primitive(content) or isinstance(content, type(None)):
             return content
         if not check_str_in_iterable("_optimizer", content):
             return content
-    
+
         optimizer = content['params']
-        return AdamOptimizer(learning_rate=optimizer['learning_rate'], beta_1=optimizer['beta_1'],
-                                 beta_2=optimizer['beta_2'], epsilon=optimizer['epsilon'])
+        return AdamOptimizer(
+            learning_rate=optimizer['learning_rate'],
+            beta_1=optimizer['beta_1'],
+            beta_2=optimizer['beta_2'],
+            epsilon=optimizer['epsilon'])
