1700b4bb70ae8b685d4d8f4799025d90fd8dbb53
AHReccese <amirhssin6rst@gmail.com>
2023-07-16 18:20:47 +0330

    doc string added

--- pymilo/transporters/sgdoptimizer_transporter.py
+++ pymilo/transporters/sgdoptimizer_transporter.py
@@ -11,7 +11,19 @@ class SGDOptimizerTransporter(AbstractTransporter):
     """Customized PyMilo Transporter developed to handle SGDOptimizer field."""
 
     def serialize(self, data, key, model_type):
+        """
+        Serialize instances of the SGDOptimizer class.
 
+        Record the `learning_rate`, `momentum`, `decay` and `nesterov` fields of random state object.
+
+        :param data: the internal data dictionary of the given model
+        :type data: dict
+        :param key: the special key of the data param, which we're going to serialize its value(data[key])
+        :type key: object
+        :param model_type: the model type of the ML model, which its data dictionary is given as the data param.
+        :type model_type: str
+        :return: pymilo serialized output of data[key]
+        """
         if ((model_type == "MLPRegressor" and key ==
              "_optimizer" and isinstance(data[key], SGDOptimizer))):
             optimizer = data[key]
@@ -26,10 +38,26 @@ class SGDOptimizerTransporter(AbstractTransporter):
         return data[key]
 
     def deserialize(self, data, key, model_type):
+        """
+        Deserialize the special _optimizer field of the SGDOptimizer.
 
+        The associated _optimizer field of the pymilo serialized model, is extracted through
+        it's previously serialized parameters.
+
+        deserialize the data[key] of the given model which type is model_type.
+        basically in order to fully deserialize a model, we should traverse over all the keys of its serialized data dictionary and
+        pass it through the chain of associated transporters to get fully deserialized.
+
+        :param data: the internal data dictionary of the associated json file of the ML model which is generated previously by
+        pymilo export.
+        :type data: dict
+        :param key: the special key of the data param, which we're going to deserialize its value(data[key])
+        :type key: object
+        :param model_type: the model type of the ML model, which internal serialized data dictionary is given as the data param
+        :type model_type: str
+        :return: pymilo deserialized output of data[key]
+        """
         content = data[key]
-        # if model_type != "MLPRegressor":
-        #    return content
         if is_primitive(content) or isinstance(content, type(None)):
             return content
         if not check_str_in_iterable("_optimizer", content):
