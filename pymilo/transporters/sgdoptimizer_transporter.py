00840a034e8c72b595ce501c398886f675772f8a
AHReccese <amirhssin6rst@gmail.com>
2023-07-16 18:00:35 +0330

    autopep8 run for sgd optimizer transporter

--- pymilo/transporters/sgdoptimizer_transporter.py
+++ pymilo/transporters/sgdoptimizer_transporter.py
@@ -6,14 +6,14 @@ from .transporter import AbstractTransporter
 
 # Handling sklearn.neural_network._stochastic_optimizers.SGDOptimizer
 
+
 class SGDOptimizerTransporter(AbstractTransporter):
     """Customized PyMilo Transporter developed to handle SGDOptimizer field."""
 
     def serialize(self, data, key, model_type):
 
-        if (
-            (model_type == "MLPRegressor" and key == "_optimizer" and isinstance(data[key], SGDOptimizer)) 
-        ):
+        if ((model_type == "MLPRegressor" and key ==
+             "_optimizer" and isinstance(data[key], SGDOptimizer))):
             optimizer = data[key]
             data[key] = {
                 'params': {
@@ -29,12 +29,15 @@ class SGDOptimizerTransporter(AbstractTransporter):
 
         content = data[key]
         # if model_type != "MLPRegressor":
-        #    return content 
+        #    return content
         if is_primitive(content) or isinstance(content, type(None)):
             return content
         if not check_str_in_iterable("_optimizer", content):
             return content
-    
+
         optimizer = content['params']
-        return SGDOptimizer(learning_rate=optimizer['learning_rate'], momentum=optimizer['momentum'],
-                                 decay=optimizer['decay'], nesterov=optimizer['nesterov'])
+        return SGDOptimizer(
+            learning_rate=optimizer['learning_rate'],
+            momentum=optimizer['momentum'],
+            decay=optimizer['decay'],
+            nesterov=optimizer['nesterov'])
