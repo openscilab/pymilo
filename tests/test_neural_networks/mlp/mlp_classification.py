1f5202fc476b9b3467fb05d2306baa3ae7d8ad99
AHReccese <amirhssin6rst@gmail.com>
2023-07-28 23:30:24 +0330

    add MlpClassifier test

---
+++ tests/test_neural_networks/mlp/mlp_classification.py
@@ -0,0 +1,17 @@
+from sklearn.neural_network import MLPClassifier
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
+
+from pymilo.utils.util import get_sklearn_type
+
+MODEL_NAME = "Multi Layer Perceptron Classification"
+
+
+def multi_layer_perceptron_classification():
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
+    # Create MLPClassifier object
+    multi_layer_perceptron_classifier = MLPClassifier(random_state=1, max_iter=500).fit(x_train, y_train)
+    # Train the model using the training sets
+    multi_layer_perceptron_classifier.fit(x_train, y_train)
+    assert pymilo_classification_test(
+        multi_layer_perceptron_classifier, MODEL_NAME, (x_test, y_test)) == True 
