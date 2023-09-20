773170a93f455f1b1c12ec1ddad93b46fe17a2d9
AHReccese <amirhssin6rst@gmail.com>
2023-09-20 16:44:01 +0330

    DecisionTreeClassifier Test added

---
+++ tests/test_decision_trees/decision_tree/decision_tree_classification.py
@@ -0,0 +1,14 @@
+from sklearn.tree import DecisionTreeClassifier
+
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
+
+MODEL_NAME = "Decision Tree Classifier"
+
+def decision_tree_classification():
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
+    # Create Decision Tree Regressor
+    decision_tree_classifier = DecisionTreeClassifier(random_state=1)
+    decision_tree_classifier = decision_tree_classifier.fit(x_train, y_train)
+    assert pymilo_classification_test(
+        decision_tree_classifier, MODEL_NAME, (x_test, y_test)) == True 
