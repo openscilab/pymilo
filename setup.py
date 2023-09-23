63bb2e595081dc0dff3f15345d7da6efb87bcb72
AHReccese <amirhssin6rst@gmail.com>
2023-09-23 18:43:00 +0330

    update `setup.py`

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'pymilo.chains',
         'pymilo.transporters',
         'pymilo.exceptions'],
-    version='0.2',
+    version='0.3',
     description='Transportation of ML models',
     long_description=read_description(),
     long_description_content_type='text/markdown',
