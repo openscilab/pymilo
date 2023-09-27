37279d9bfee0f43c472fdce76f90285d39972b55
AHReccese <amirhssin6rst@gmail.com>
2023-09-27 12:45:55 +0330

    clustering's tests runner added

---
+++ tests/test_clusterings/test_clusterings.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+
+from kmeans import kmeans
+from affinity_propagation import affinity_propagation
+from mean_shift import mean_shift
+
+CLUSTERINGS = {
+    "KMEANS": [kmeans],
+    "AFFINITY_PROPAGATION": [affinity_propagation],
+    "MEAN_SHIFT": [mean_shift]
+}
+
+@pytest.fixture(scope="session", autouse=True)
+def reset_exported_models_directory():
+    exported_models_directory = os.path.join(
+        os.getcwd(), "tests", "exported_clusterings")
+    if not os.path.isdir(exported_models_directory):
+        os.mkdir(exported_models_directory)
+        return
+    for file_name in os.listdir(exported_models_directory):
+        # construct full file path
+        json_file = os.path.join(exported_models_directory, file_name)
+        if os.path.isfile(json_file):
+            os.remove(json_file)
+
+def test_full():
+    for category in CLUSTERINGS.keys():
+        for model in CLUSTERINGS[category]:
+            model()
