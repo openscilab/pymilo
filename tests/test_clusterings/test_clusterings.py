31f5799615b15f355c07fe4cf98414088173516a
AHReccese <amirhssin6rst@gmail.com>
2023-09-27 12:54:52 +0330

    remove future models imports

--- tests/test_clusterings/test_clusterings.py
+++ tests/test_clusterings/test_clusterings.py
@@ -2,13 +2,9 @@ import os
 import pytest
 
 from kmeans import kmeans
-from affinity_propagation import affinity_propagation
-from mean_shift import mean_shift
 
 CLUSTERINGS = {
     "KMEANS": [kmeans],
-    "AFFINITY_PROPAGATION": [affinity_propagation],
-    "MEAN_SHIFT": [mean_shift]
 }
 
 @pytest.fixture(scope="session", autouse=True)
