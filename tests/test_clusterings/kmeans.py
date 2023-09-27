ab072f3050ed6f0edeac085b6701e1f34475ec56
AHReccese <amirhssin6rst@gmail.com>
2023-09-27 12:45:27 +0330

    kmeans test added

---
+++ tests/test_clusterings/kmeans.py
@@ -0,0 +1,11 @@
+from sklearn.cluster import KMeans
+
+from pymilo.utils.test_pymilo import pymilo_clustering_test
+from pymilo.utils.data_exporter import prepare_simple_clustering_datasets
+
+MODEL_NAME = "Kmeans"
+
+def kmeans():    
+    x, y = prepare_simple_clustering_datasets()
+    kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(x, y)
+    pymilo_clustering_test(kmeans, MODEL_NAME, x)
