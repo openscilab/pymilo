f00eb3532b1fb626fe42200654fcf14463c3c688
AHReccese <amirhssin6rst@gmail.com>
2023-10-30 07:06:27 +0330

    add `MeanShift` test to clustering test runner

--- tests/test_clusterings/test_clusterings.py
+++ tests/test_clusterings/test_clusterings.py
@@ -3,10 +3,12 @@ import pytest
 
 from kmeans import kmeans
 from affinity_propagation import affinity_propagation
+from mean_shift import mean_shift
 
 CLUSTERINGS = {
     "KMEANS": [kmeans],
     "AFFINITY_PROPAGATION": [affinity_propagation],
+    "MEAN_SHIFT": [mean_shift],
 }
 
 @pytest.fixture(scope="session", autouse=True)
