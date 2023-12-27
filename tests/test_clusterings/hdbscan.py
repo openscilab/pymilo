dc7dfa57612c5d8a417ee956db730d7db9a0e061
AHReccese <amirhssin6rst@gmail.com>
2023-12-27 20:50:35 +0330

    apply autopep8.sh in `hdbscan.py`

--- tests/test_clusterings/hdbscan.py
+++ tests/test_clusterings/hdbscan.py
@@ -5,7 +5,7 @@ from pymilo.utils.data_exporter import prepare_simple_clustering_datasets
 
 MODEL_NAME = "HDBSCAN"
 
-def hdbscan():    
+def hdbscan():
     x, y = prepare_simple_clustering_datasets()
     hdbscan = HDBSCAN(min_cluster_size=20).fit(x, y)
     pymilo_clustering_test(hdbscan, MODEL_NAME, x)
