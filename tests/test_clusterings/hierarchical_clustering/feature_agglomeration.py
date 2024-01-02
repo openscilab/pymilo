e1a45e64b2192e96347b96f4f5bb94cff0773468
AmirHosein Rostami <32750909+AHReccese@users.noreply.github.com>
2024-01-02 19:58:14 +0330

    Update feature_agglomeration.py
    Remove trailing white spaces from function declaration

--- tests/test_clusterings/hierarchical_clustering/feature_agglomeration.py
+++ tests/test_clusterings/hierarchical_clustering/feature_agglomeration.py
@@ -5,7 +5,7 @@ from pymilo.utils.data_exporter import prepare_simple_clustering_datasets
 
 MODEL_NAME = "Feature Agglomeration"
 
-def feature_agglomeration():    
+def feature_agglomeration():
     x, y = prepare_simple_clustering_datasets()
     feature_agglomeration = FeatureAgglomeration(n_clusters=2).fit(x, y)
     pymilo_clustering_test(feature_agglomeration, MODEL_NAME, x)
