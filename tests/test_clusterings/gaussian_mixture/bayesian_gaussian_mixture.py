2fd9f3f9ce3907fc0f12283ad1cd6822bc7ea9a2
AHReccese <amirhssin6rst@gmail.com>
2024-01-03 07:58:48 +0330

    remove trailing whitespaces

--- tests/test_clusterings/gaussian_mixture/bayesian_gaussian_mixture.py
+++ tests/test_clusterings/gaussian_mixture/bayesian_gaussian_mixture.py
@@ -5,7 +5,7 @@ from pymilo.utils.data_exporter import prepare_simple_clustering_datasets
 
 MODEL_NAME = "Bayesian Gaussian Mixture"
 
-def bayesian_gaussian_mixture():    
+def bayesian_gaussian_mixture():
     x, y = prepare_simple_clustering_datasets()
     bayesian_gaussian_mixture = BayesianGaussianMixture(n_components=2, random_state=42).fit(x, y)
     pymilo_clustering_test(bayesian_gaussian_mixture, MODEL_NAME, x)
