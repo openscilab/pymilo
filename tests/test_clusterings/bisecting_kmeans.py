3161a3e885fedbe246655484785ac9770a6a9cfe
AHReccese <amirhssin6rst@gmail.com>
2024-01-06 21:09:03 +0330

    enhance `BisectingKMeans` test

--- tests/test_clusterings/bisecting_kmeans.py
+++ tests/test_clusterings/bisecting_kmeans.py
@@ -2,10 +2,11 @@ from sklearn.cluster import BisectingKMeans
 
 from pymilo.utils.test_pymilo import pymilo_clustering_test
 from pymilo.utils.data_exporter import prepare_simple_clustering_datasets
+from pymilo.utils.test_pymilo import pymilo_test
 
 MODEL_NAME = "Bisecting KMeans"
 
 def bisecting_kmeans():
     x, y = prepare_simple_clustering_datasets()
     bisecting_kmeans = BisectingKMeans(n_clusters=3, random_state=0).fit(x, y)
-    pymilo_clustering_test(bisecting_kmeans, MODEL_NAME, x)
+    pymilo_clustering_test(bisecting_kmeans, MODEL_NAME, x, True)
