3677ac44815e5f397921a5ad151ca1f092a4e94b
AHReccese <amirhssin6rst@gmail.com>
2024-01-14 05:12:42 +0330

    remove `n_init` field concrete init

--- tests/test_clusterings/minibatch_kmeans.py
+++ tests/test_clusterings/minibatch_kmeans.py
@@ -7,5 +7,5 @@ MODEL_NAME = "MiniBatch KMeans"
 
 def minibatch_kmeans():
     x, y = prepare_simple_clustering_datasets()
-    minibatch_kmeans = MiniBatchKMeans(n_clusters=2, random_state=2, batch_size=6, max_iter=10, n_init="auto").fit(x, y)
+    minibatch_kmeans = MiniBatchKMeans(n_clusters=2, random_state=2, batch_size=6, max_iter=10).fit(x, y)
     pymilo_clustering_test(minibatch_kmeans, MODEL_NAME, x, True)
