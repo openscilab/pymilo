69e1bb0b6862b03f8cd425e848623c955654ed5a
AHReccese <amirhssin6rst@gmail.com>
2024-01-14 04:48:28 +0330

    `SpectralBiclustering` test added

---
+++ tests/test_clusterings/spectral_clustering/spectral_biclustering.py
@@ -0,0 +1,11 @@
+from sklearn.cluster import SpectralBiclustering
+
+from pymilo.utils.test_pymilo import pymilo_clustering_test
+from pymilo.utils.data_exporter import prepare_simple_clustering_datasets
+
+MODEL_NAME = "Spectral Biclustering"
+
+def spectral_biclustering():    
+    x, y = prepare_simple_clustering_datasets()
+    spectral_biclustering = SpectralBiclustering(n_clusters=2, random_state=0).fit(x, y)
+    pymilo_clustering_test(spectral_biclustering, MODEL_NAME, x)
