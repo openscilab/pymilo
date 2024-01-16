90629701eb4c92b657a3368e76a0f49400856ec8
AHReccese <amirhssin6rst@gmail.com>
2024-01-16 09:43:00 +0330

    remove unused variables

--- tests/test_neural_networks/mlp/mlp_regression.py
+++ tests/test_neural_networks/mlp/mlp_regression.py
@@ -7,9 +7,6 @@ MODEL_NAME = "Multi Layer Perceptron Regression"
 
 def multi_layer_perceptron_regression():
     x_train, y_train, x_test, y_test = prepare_simple_regression_datasets()
-    # Create Passive Aggressive Regression object
-    par_random_state = 2
-    par_max_iter = 100
     multi_layer_perceptron_regression = MLPRegressor(random_state=1, max_iter=500).fit(x_train, y_train)
     # Train the model using the training sets
     multi_layer_perceptron_regression.fit(x_train, y_train)
