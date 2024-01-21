6ad049b6ac115e1b7a9ec0566edfaa4dfd5fed0f
AHReccese <amirhssin6rst@gmail.com>
2024-01-21 12:20:54 +0330

    `ComplementNB` test added

---
+++ tests/test_naive_bayes/complement.py
@@ -0,0 +1,11 @@
+from sklearn.naive_bayes import ComplementNB
+
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
+
+MODEL_NAME = "ComplementNB"
+
+def complement_naive_bayes():
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
+    complement_naive_bayes = ComplementNB(force_alpha=True).fit(x_train, y_train)
+    pymilo_classification_test(complement_naive_bayes, MODEL_NAME, (x_test, y_test))
