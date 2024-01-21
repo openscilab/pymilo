83d883d75327d14d826bb825f8d4a9d80e6e1771
AHReccese <amirhssin6rst@gmail.com>
2024-01-21 12:20:40 +0330

    `MultinomialNB` test added

---
+++ tests/test_naive_bayes/multinomial.py
@@ -0,0 +1,11 @@
+from sklearn.naive_bayes import MultinomialNB
+
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
+
+MODEL_NAME = "MultinomialNB"
+
+def multinomial_naive_bayes():
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
+    multinomial_naive_bayes = MultinomialNB(force_alpha=True).fit(x_train, y_train)
+    pymilo_classification_test(multinomial_naive_bayes, MODEL_NAME, (x_test, y_test))
