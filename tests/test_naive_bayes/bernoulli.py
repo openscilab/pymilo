149abb452f9f42199e705b33f4542b11724da17b
AHReccese <amirhssin6rst@gmail.com>
2024-01-21 12:20:08 +0330

    `BernoulliNB` test added

---
+++ tests/test_naive_bayes/bernoulli.py
@@ -0,0 +1,11 @@
+from sklearn.naive_bayes import BernoulliNB
+
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
+
+MODEL_NAME = "BernoulliNB"
+
+def bernoulli_naive_bayes():
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
+    bernoulli_naive_bayes = BernoulliNB(force_alpha=True).fit(x_train, y_train)
+    pymilo_classification_test(bernoulli_naive_bayes, MODEL_NAME, (x_test, y_test))
