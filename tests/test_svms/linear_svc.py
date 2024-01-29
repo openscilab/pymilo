06a186be76f0a263f0fff3e370920ebbb4817bb8
AHReccese <amirhssin6rst@gmail.com>
2024-01-29 06:25:52 +0330

    add LinearSVC test

---
+++ tests/test_svms/linear_svc.py
@@ -0,0 +1,11 @@
+from sklearn.svm import LinearSVC
+
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
+
+MODEL_NAME = "LinearSVC"
+
+def linear_svc():
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
+    linear_svc = LinearSVC(dual="auto", random_state=0, tol=1e-5).fit(x_train, y_train)
+    pymilo_classification_test(linear_svc, MODEL_NAME, (x_test, y_test))
