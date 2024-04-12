fe4cf4263638cad1322d05b9415b106e479719a5
AHReccese <amirhssin6rst@gmail.com>
2024-04-12 14:48:40 +0330

    apply refactorings

--- pymilo/pymilo_param.py
+++ pymilo/pymilo_param.py
@@ -1,115 +1,6 @@
 # -*- coding: utf-8 -*-
 """Parameters and constants."""
-from sklearn.mixture import BayesianGaussianMixture
-from sklearn.mixture import GaussianMixture
-from sklearn.cluster import OPTICS
-from sklearn.cluster import Birch
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import FeatureAgglomeration
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import SpectralCoclustering
-from sklearn.cluster import SpectralBiclustering
-from sklearn.cluster import SpectralClustering
-from sklearn.cluster import MeanShift
-from sklearn.cluster import AffinityPropagation
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.cluster import KMeans
-from sklearn.tree import ExtraTreeClassifier
-from sklearn.tree import ExtraTreeRegressor
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.neural_network import BernoulliRBM
-from sklearn.neural_network import MLPClassifier
-from sklearn.neural_network import MLPRegressor
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import BaggingRegressor
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import IsolationForest
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomTreesEmbedding
-from sklearn.ensemble import StackingRegressor
-from sklearn.ensemble import StackingClassifier
-from sklearn.ensemble import VotingRegressor
-from sklearn.ensemble import VotingClassifier
-
-hist_gradient_boosting_support = False
-try:
-    from sklearn.ensemble import HistGradientBoostingRegressor
-    from sklearn.ensemble import HistGradientBoostingClassifier
-    hist_gradient_boosting_support = True
-except BaseException:
-    pass
-
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import RadiusNeighborsRegressor
-from sklearn.neighbors import RadiusNeighborsClassifier
-from sklearn.neighbors import NearestNeighbors
-from sklearn.neighbors import NearestCentroid
-from sklearn.neighbors import LocalOutlierFactor
-
-from sklearn.svm import SVR
-from sklearn.svm import SVC
-from sklearn.svm import OneClassSVM
-from sklearn.svm import NuSVR
-from sklearn.svm import NuSVC
-from sklearn.svm import LinearSVR
-from sklearn.svm import LinearSVC
-
-from sklearn.naive_bayes import CategoricalNB
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import ComplementNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import GaussianNB
-
-from sklearn.preprocessing import LabelBinarizer
-from numpy import uint8
-from numpy import intc
-from numpy import inf
-from numpy import float64
-from numpy import int32
-from numpy import int64
-from numpy import uint64
-
-from sklearn.dummy import DummyRegressor
-from sklearn.dummy import DummyClassifier
-
-from sklearn.linear_model import HuberRegressor
-from sklearn.linear_model import TheilSenRegressor
-from sklearn.linear_model import RANSACRegressor
-from sklearn.linear_model import PassiveAggressiveRegressor
-from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.linear_model import SGDRegressor
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import ARDRegression
-from sklearn.linear_model import BayesianRidge
-from sklearn.linear_model import OrthogonalMatchingPursuitCV
-from sklearn.linear_model import OrthogonalMatchingPursuit
-from sklearn.linear_model import MultiTaskElasticNetCV
-from sklearn.linear_model import MultiTaskElasticNet
-from sklearn.linear_model import ElasticNetCV
-from sklearn.linear_model import ElasticNet
-from sklearn.linear_model import MultiTaskLassoCV
-from sklearn.linear_model import MultiTaskLasso
-from sklearn.linear_model import LassoLarsIC
-from sklearn.linear_model import LassoLarsCV
-from sklearn.linear_model import LassoLars
-from sklearn.linear_model import LassoCV
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import RidgeClassifierCV
-from sklearn.linear_model import RidgeClassifier
-from sklearn.linear_model import RidgeCV
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import LinearRegression
+import sklearn.linear_model as linear_model
 
 quantile_regressor_support = False
 try:
@@ -133,7 +24,6 @@ try:
 except BaseException:
     pass
 
-
 sgd_one_class_svm_support = False
 try:
     from sklearn.linear_model import SGDOneClassSVM
@@ -141,7 +31,10 @@ try:
 except BaseException:
     pass
 
+import sklearn.neural_network as neural_network
+import sklearn.tree as tree
 
+import sklearn.cluster as cluster
 bisecting_kmeans_support = False
 try:
     from sklearn.cluster import BisectingKMeans
@@ -156,128 +49,154 @@ try:
 except BaseException:
     pass
 
+import sklearn.mixture as mixture
+import sklearn.naive_bayes as naive_bayes
+import sklearn.svm as svm
+import sklearn.neighbors as neighbors
+import sklearn.dummy as dummy
+
+import sklearn.ensemble as ensemble
+hist_gradient_boosting_support = False
+try:
+    from sklearn.ensemble import HistGradientBoostingRegressor
+    from sklearn.ensemble import HistGradientBoostingClassifier
+    hist_gradient_boosting_support = True
+except BaseException:
+    pass
+
+
+from numpy import uint64
+from numpy import int64
+from numpy import int32
+from numpy import float64
+from numpy import inf
+from numpy import intc
+from numpy import uint8
+
+from sklearn.preprocessing import LabelBinarizer
+
 PYMILO_VERSION = "0.7"
 NOT_SUPPORTED = "NOT_SUPPORTED"
 PYMILO_VERSION_DOES_NOT_EXIST = "Corrupted JSON file, `pymilo_version` doesn't exist in this file."
 UNEQUAL_PYMILO_VERSIONS = "warning: Installed Pymilo version differes from pymilo version used to create the JSON file."
 
 SKLEARN_LINEAR_MODEL_TABLE = {
-    "DummyRegressor": DummyRegressor,
-    "DummyClassifier": DummyClassifier,
-    "LinearRegression": LinearRegression,
-    "Ridge": Ridge,
-    "RidgeCV": RidgeCV,
-    "RidgeClassifier": RidgeClassifier,
-    "RidgeClassifierCV": RidgeClassifierCV,
-    "Lasso": Lasso,
-    "LassoCV": LassoCV,
-    "LassoLars": LassoLars,
-    "LassoLarsCV": LassoLarsCV,
-    "LassoLarsIC": LassoLarsIC,
-    "MultiTaskLasso": MultiTaskLasso,
-    "MultiTaskLassoCV": MultiTaskLassoCV,
-    "ElasticNet": ElasticNet,
-    "ElasticNetCV": ElasticNetCV,
-    "MultiTaskElasticNet": MultiTaskElasticNet,
-    "MultiTaskElasticNetCV": MultiTaskElasticNetCV,
-    "OrthogonalMatchingPursuit": OrthogonalMatchingPursuit,
-    "OrthogonalMatchingPursuitCV": OrthogonalMatchingPursuitCV,
-    "BayesianRidge": BayesianRidge,
-    "ARDRegression": ARDRegression,
-    "LogisticRegression": LogisticRegression,
-    "LogisticRegressionCV": LogisticRegressionCV,
+    "DummyRegressor": dummy.DummyRegressor,
+    "DummyClassifier": dummy.DummyClassifier,
+    "LinearRegression": linear_model.LinearRegression,
+    "Ridge": linear_model.Ridge,
+    "RidgeCV": linear_model.RidgeCV,
+    "RidgeClassifier": linear_model.RidgeClassifier,
+    "RidgeClassifierCV": linear_model.RidgeClassifierCV,
+    "Lasso": linear_model.Lasso,
+    "LassoCV": linear_model.LassoCV,
+    "LassoLars": linear_model.LassoLars,
+    "LassoLarsCV": linear_model.LassoLarsCV,
+    "LassoLarsIC": linear_model.LassoLarsIC,
+    "MultiTaskLasso": linear_model.MultiTaskLasso,
+    "MultiTaskLassoCV": linear_model.MultiTaskLassoCV,
+    "ElasticNet": linear_model.ElasticNet,
+    "ElasticNetCV": linear_model.ElasticNetCV,
+    "MultiTaskElasticNet": linear_model.MultiTaskElasticNet,
+    "MultiTaskElasticNetCV": linear_model.MultiTaskElasticNetCV,
+    "OrthogonalMatchingPursuit": linear_model.OrthogonalMatchingPursuit,
+    "OrthogonalMatchingPursuitCV": linear_model.OrthogonalMatchingPursuitCV,
+    "BayesianRidge": linear_model.BayesianRidge,
+    "ARDRegression": linear_model.ARDRegression,
+    "LogisticRegression": linear_model.LogisticRegression,
+    "LogisticRegressionCV": linear_model.LogisticRegressionCV,
     "TweedieRegressor": TweedieRegressor if glm_support['TweedieRegressor'] else NOT_SUPPORTED,
     "PoissonRegressor": PoissonRegressor if glm_support['PoissonRegressor'] else NOT_SUPPORTED,
     "GammaRegressor": GammaRegressor if glm_support['GammaRegressor'] else NOT_SUPPORTED,
-    "SGDRegressor": SGDRegressor,
-    "SGDClassifier": SGDClassifier,
+    "SGDRegressor": linear_model.SGDRegressor,
+    "SGDClassifier": linear_model.SGDClassifier,
     "SGDOneClassSVM": SGDOneClassSVM if sgd_one_class_svm_support else NOT_SUPPORTED,
-    "Perceptron": Perceptron,
-    "PassiveAggressiveRegressor": PassiveAggressiveRegressor,
-    "PassiveAggressiveClassifier": PassiveAggressiveClassifier,
-    "RANSACRegressor": RANSACRegressor,
-    "TheilSenRegressor": TheilSenRegressor,
-    "HuberRegressor": HuberRegressor,
+    "Perceptron": linear_model.Perceptron,
+    "PassiveAggressiveRegressor": linear_model.PassiveAggressiveRegressor,
+    "PassiveAggressiveClassifier": linear_model.PassiveAggressiveClassifier,
+    "RANSACRegressor": linear_model.RANSACRegressor,
+    "TheilSenRegressor": linear_model.TheilSenRegressor,
+    "HuberRegressor": linear_model.HuberRegressor,
     "QuantileRegressor": QuantileRegressor if quantile_regressor_support else NOT_SUPPORTED,
 }
 
 SKLEARN_NEURAL_NETWORK_TABLE = {
-    "MLPRegressor": MLPRegressor,
-    "MLPClassifier": MLPClassifier,
-    "BernoulliRBM": BernoulliRBM,
+    "MLPRegressor": neural_network.MLPRegressor,
+    "MLPClassifier": neural_network.MLPClassifier,
+    "BernoulliRBM": neural_network.BernoulliRBM,
 }
 
 SKLEARN_DECISION_TREE_TABLE = {
-    "DecisionTreeRegressor": DecisionTreeRegressor,
-    "DecisionTreeClassifier": DecisionTreeClassifier,
-    "ExtraTreeRegressor": ExtraTreeRegressor,
-    "ExtraTreeClassifier": ExtraTreeClassifier
+    "DecisionTreeRegressor": tree.DecisionTreeRegressor,
+    "DecisionTreeClassifier": tree.DecisionTreeClassifier,
+    "ExtraTreeRegressor": tree.ExtraTreeRegressor,
+    "ExtraTreeClassifier": tree.ExtraTreeClassifier
 }
 
 SKLEARN_CLUSTERING_TABLE = {
-    "KMeans": KMeans,
-    "MiniBatchKMeans": MiniBatchKMeans,
+    "KMeans": cluster.KMeans,
+    "MiniBatchKMeans": cluster.MiniBatchKMeans,
     "BisectingKMeans": BisectingKMeans if bisecting_kmeans_support else NOT_SUPPORTED,
-    "AffinityPropagation": AffinityPropagation,
-    "MeanShift": MeanShift,
-    "SpectralClustering": SpectralClustering,
-    "SpectralBiclustering": SpectralBiclustering,
-    "SpectralCoclustering": SpectralCoclustering,
-    "AgglomerativeClustering": AgglomerativeClustering,
-    "FeatureAgglomeration": FeatureAgglomeration,
-    "DBSCAN": DBSCAN,
+    "AffinityPropagation": cluster.AffinityPropagation,
+    "MeanShift": cluster.MeanShift,
+    "SpectralClustering": cluster.SpectralClustering,
+    "SpectralBiclustering": cluster.SpectralBiclustering,
+    "SpectralCoclustering": cluster.SpectralCoclustering,
+    "AgglomerativeClustering": cluster.AgglomerativeClustering,
+    "FeatureAgglomeration": cluster.FeatureAgglomeration,
+    "DBSCAN": cluster.DBSCAN,
     "HDBSCAN": HDBSCAN if hdbscan_support else NOT_SUPPORTED,
-    "OPTICS": OPTICS,
-    "Birch": Birch,
-    "GaussianMixture": GaussianMixture,
-    "BayesianGaussianMixture": BayesianGaussianMixture,
+    "OPTICS": cluster.OPTICS,
+    "Birch": cluster.Birch,
+    "GaussianMixture": mixture.GaussianMixture,
+    "BayesianGaussianMixture": mixture.BayesianGaussianMixture,
 }
 
 SKLEARN_NAIVE_BAYES_TABLE = {
-    "GaussianNB": GaussianNB,
-    "MultinomialNB": MultinomialNB,
-    "ComplementNB": ComplementNB,
-    "BernoulliNB": BernoulliNB,
-    "CategoricalNB": CategoricalNB,
+    "GaussianNB": naive_bayes.GaussianNB,
+    "MultinomialNB": naive_bayes.MultinomialNB,
+    "ComplementNB": naive_bayes.ComplementNB,
+    "BernoulliNB": naive_bayes.BernoulliNB,
+    "CategoricalNB": naive_bayes.CategoricalNB,
 }
 
 SKLEARN_SVM_TABLE = {
-    "LinearSVC": LinearSVC,
-    "LinearSVR": LinearSVR,
-    "NuSVC": NuSVC,
-    "NuSVR": NuSVR,
-    "OneClassSVM": OneClassSVM,
-    "SVC": SVC,
-    "SVR": SVR,
+    "LinearSVC": svm.LinearSVC,
+    "LinearSVR": svm.LinearSVR,
+    "NuSVC": svm.NuSVC,
+    "NuSVR": svm.NuSVR,
+    "OneClassSVM": svm.OneClassSVM,
+    "SVC": svm.SVC,
+    "SVR": svm.SVR,
 }
 
 SKLEARN_NEIGHBORS_TABLE = {
-    "KNeighborsRegressor": KNeighborsRegressor,
-    "KNeighborsClassifier": KNeighborsClassifier,
-    "RadiusNeighborsRegressor": RadiusNeighborsRegressor,
-    "RadiusNeighborsClassifier": RadiusNeighborsClassifier,
-    "NearestNeighbors": NearestNeighbors,
-    "NearestCentroid": NearestCentroid,
-    "LocalOutlierFactor": LocalOutlierFactor,
+    "KNeighborsRegressor": neighbors.KNeighborsRegressor,
+    "KNeighborsClassifier": neighbors.KNeighborsClassifier,
+    "RadiusNeighborsRegressor": neighbors.RadiusNeighborsRegressor,
+    "RadiusNeighborsClassifier": neighbors.RadiusNeighborsClassifier,
+    "NearestNeighbors": neighbors.NearestNeighbors,
+    "NearestCentroid": neighbors.NearestCentroid,
+    "LocalOutlierFactor": neighbors.LocalOutlierFactor,
 }
 
 SKLEARN_ENSEMBLE_TABLE = {
-    "AdaBoostRegressor": AdaBoostRegressor,
-    "AdaBoostClassifier": AdaBoostClassifier,
-    "BaggingRegressor": BaggingRegressor,
-    "BaggingClassifier": BaggingClassifier,
-    "ExtraTreesClassifier": ExtraTreesClassifier,
-    "ExtraTreesRegressor": ExtraTreesRegressor,
-    "GradientBoostingRegressor": GradientBoostingRegressor,
-    "GradientBoostingClassifier": GradientBoostingClassifier,
-    "IsolationForest": IsolationForest,
-    "RandomForestClassifier": RandomForestClassifier,
-    "RandomForestRegressor": RandomForestRegressor,
-    "RandomTreesEmbedding": RandomTreesEmbedding,
-    "StackingRegressor": StackingRegressor,
-    "StackingClassifier": StackingClassifier,
-    "VotingRegressor": VotingRegressor,
-    "VotingClassifier": VotingClassifier,
+    "AdaBoostRegressor": ensemble.AdaBoostRegressor,
+    "AdaBoostClassifier": ensemble.AdaBoostClassifier,
+    "BaggingRegressor": ensemble.BaggingRegressor,
+    "BaggingClassifier": ensemble.BaggingClassifier,
+    "ExtraTreesClassifier": ensemble.ExtraTreesClassifier,
+    "ExtraTreesRegressor": ensemble.ExtraTreesRegressor,
+    "GradientBoostingRegressor": ensemble.GradientBoostingRegressor,
+    "GradientBoostingClassifier": ensemble.GradientBoostingClassifier,
+    "IsolationForest": ensemble.IsolationForest,
+    "RandomForestClassifier": ensemble.RandomForestClassifier,
+    "RandomForestRegressor": ensemble.RandomForestRegressor,
+    "RandomTreesEmbedding": ensemble.RandomTreesEmbedding,
+    "StackingRegressor": ensemble.StackingRegressor,
+    "StackingClassifier": ensemble.StackingClassifier,
+    "VotingRegressor": ensemble.VotingRegressor,
+    "VotingClassifier": ensemble.VotingClassifier,
     "HistGradientBoostingRegressor": HistGradientBoostingRegressor if hist_gradient_boosting_support else NOT_SUPPORTED,
     "HistGradientBoostingClassifier": HistGradientBoostingClassifier if hist_gradient_boosting_support else NOT_SUPPORTED,
 }
