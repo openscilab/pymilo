983ddca52710510cc06c7230f109d6ee39d84abe
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-05-12 13:40:53 -0700

    update : preprocessing tests updated.

--- tests/test_preprocessings/test_preprocessings.py
+++ tests/test_preprocessings/test_preprocessings.py
@@ -1,10 +1,14 @@
-from binarizer import binarizer
 from one_hot_encoder import one_hot_encoder
 from label_binarizer import label_binarizer
 from label_encoder import label_encoder
 from standard_scaler import standard_scaler
+from binarizer import binarizer
 
-PREPROCESSINGS = [binarizer, one_hot_encoder, label_binarizer, label_encoder, standard_scaler]
+PREPROCESSINGS = [one_hot_encoder,
+                  label_binarizer,
+                  label_encoder,
+                  standard_scaler,
+                  binarizer]
 
 def test_full():
     for pre in PREPROCESSINGS:
