8058815fe6f74fd15cd26e150bb0d53a916fc107
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-05-12 13:39:11 -0700

    add : tests added for `StandardScaler`.

--- tests/test_preprocessings/test_preprocessings.py
+++ tests/test_preprocessings/test_preprocessings.py
@@ -2,8 +2,9 @@ from binarizer import binarizer
 from one_hot_encoder import one_hot_encoder
 from label_binarizer import label_binarizer
 from label_encoder import label_encoder
+from standard_scaler import standard_scaler
 
-PREPROCESSINGS = [binarizer, one_hot_encoder, label_binarizer, label_encoder]
+PREPROCESSINGS = [binarizer, one_hot_encoder, label_binarizer, label_encoder, standard_scaler]
 
 def test_full():
     for pre in PREPROCESSINGS:
