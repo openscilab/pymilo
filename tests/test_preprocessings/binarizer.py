589ff15479753fe6f912da660c6e73fdde679ec7
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-05-12 13:16:58 -0700

    update : extra lines adde to `binarizer.py` added.

--- tests/test_preprocessings/binarizer.py
+++ tests/test_preprocessings/binarizer.py
@@ -9,13 +9,16 @@ def binarizer():
     X = [[ 1., -1.,  2.],
         [ 2.,  0.,  0.],
         [ 0.,  1., -1.]]
+    
     _binarizer = Binarizer().fit(X)
     pre_result = _binarizer.transform(X)
+    
     pt = PreprocessingTransporter()
     post_pymilo_pre_model = pt.deserialize_pre_module(
         pt.serialize_pre_module(_binarizer)
     )
     post_result = post_pymilo_pre_model.transform(X)
+    
     comparison_result = array_equal(pre_result, post_result)
     report_status(comparison_result, MODEL_NAME)
     assert comparison_result
