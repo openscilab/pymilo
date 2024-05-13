47828ee7a41d986637948c08461a2bcd30921e90
AHReccese <amirhssin6rst@gmail.com>
2024-05-13 03:57:44 +0330

    storage directory added containing preprocessings exported files in json

--- tests/test_preprocessings/test_preprocessings.py
+++ tests/test_preprocessings/test_preprocessings.py
@@ -1,3 +1,6 @@
+import os
+import pytest
+
 from one_hot_encoder import one_hot_encoder
 from label_binarizer import label_binarizer
 from label_encoder import label_encoder
@@ -10,7 +13,21 @@ PREPROCESSINGS = [one_hot_encoder,
                   label_encoder,
                   standard_scaler,
                   binarizer,
-                  function_transformer]
+                  function_transformer
+                  ]
+
+@pytest.fixture(scope="session", autouse=True)
+def reset_exported_models_directory():
+    exported_models_directory = os.path.join(
+        os.getcwd(), "tests", "exported_preprocessings")
+    if not os.path.isdir(exported_models_directory):
+        os.mkdir(exported_models_directory)
+        return
+    for file_name in os.listdir(exported_models_directory):
+        # construct full file path
+        json_file = os.path.join(exported_models_directory, file_name)
+        if os.path.isfile(json_file):
+            os.remove(json_file)
 
 def test_full():
     for pre in PREPROCESSINGS:
