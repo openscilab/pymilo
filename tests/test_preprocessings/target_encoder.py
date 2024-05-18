8ebc4c560386561919de444891ed87588b82d546
AHReccese <amirhssin6rst@gmail.com>
2024-05-19 00:59:04 +0330

    enhance `target_encoder` to be comparable before after pymiloing

--- tests/test_preprocessings/target_encoder.py
+++ tests/test_preprocessings/target_encoder.py
@@ -10,15 +10,16 @@ def target_encoder():
     X = array([["dog"] * 20 + ["cat"] * 30 + ["snake"] * 38], dtype=object).T
     y = [90.3] * 5 + [80.1] * 15 + [20.4] * 5 + [20.1] * 25 + [21.2] * 8 + [49] * 30
     enc_auto = TargetEncoder(smooth="auto")
-    pre_result = enc_auto.fit_transform(X, y)
+    enc_auto = enc_auto.fit(X, y)
+    pre_result = enc_auto.transform(X)
 
     pt = PreprocessingTransporter()
     post_pymilo_pre_model = pt.deserialize_pre_module(
         write_and_read(
             pt.serialize_pre_module(enc_auto),
             get_path(MODEL_NAME)))
-    post_result = post_pymilo_pre_model.transform(X)
 
+    post_result = post_pymilo_pre_model.transform(X)
     comparison_result = array_equal(pre_result, post_result)
     report_status(comparison_result, MODEL_NAME)
     assert comparison_result
