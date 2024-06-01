3d7dd6ce484d0484289f634cd5c3249cacb1bef4
AHReccese <amirhssin6rst@ngmail.com>
2024-06-01 03:54:37 +0330

    remove unused import has_named_parameter

--- pymilo/transporters/general_data_structure_transporter.py
+++ pymilo/transporters/general_data_structure_transporter.py
@@ -6,7 +6,7 @@ from ast import literal_eval
 from ..pymilo_param import NUMPY_TYPE_DICT
 
 from ..utils.util import get_homogeneous_type, all_same, prefix_list
-from ..utils.util import is_primitive, check_str_in_iterable, has_named_parameter
+from ..utils.util import is_primitive, check_str_in_iterable
 
 from .transporter import AbstractTransporter
 
