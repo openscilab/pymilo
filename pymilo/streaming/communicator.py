55b01b3216357c6f658960de67b6550c609d7dee
AHReccese <amirhssin6rst@gmail.com>
2024-08-03 20:46:29 -0400

    implement `RESTClientCommunicator`

--- pymilo/streaming/communicator.py
+++ pymilo/streaming/communicator.py
@@ -1,8 +1,17 @@
 from .interfaces import Communicator
 
 
-class DummyCommunicator(Communicator):
-    def send(string):
-        return string 
-    def receive(string):
-        return string
+class RESTClientCommunicator(Communicator):
+
+    def __init__(self, server_url):
+        self._server_url = server_url
+
+    def download(self, payload):
+        return requests.get(url=self._server_url + "/download/", json=payload)
+
+    def upload(self, payload):
+        return requests.post(url=self._server_url + "/upload/", json=payload)
+
+    def attribute_call(self, payload):
+        return requests.post(url=self._server_url + "/attribute_call/", json=payload)
+
