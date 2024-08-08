16fda077517735f724432a5dcf28f6fcb32a5792
AHReccese <amirhssin6rst@gmail.com>
2024-08-08 17:09:54 -0400

    remove unnecessary `pass` from @abstractmethod

--- pymilo/streaming/interfaces.py
+++ pymilo/streaming/interfaces.py
@@ -7,34 +7,41 @@ class Compressor(ABC):
 
     @abstractmethod
     def compress(payload):
-        pass
+        """
+        """
 
     @abstractmethod
     def extract(payload):
-        pass
+        """
+        """
 
 
 class Encryptor(ABC):
 
     @abstractmethod
     def encrypt(payload):
-        pass
+        """
+        """
 
     @abstractmethod
     def decrypt(payload):
-        pass
+        """
+        """
 
 
 class ClientCommunicator(ABC):
 
     @abstractmethod
     def upload(self, payload):
-        pass
+        """
+        """
 
     @abstractmethod
     def download(self, payload):
-        pass
+        """
+        """
 
     @abstractmethod
     def attribute_call(self, payload):
-        pass
+        """
+        """
