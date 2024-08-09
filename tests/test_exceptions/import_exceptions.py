d09aa62b2a67d2d3c0dc0a1e18fdf0e021e8b5fa
AHReccese <amirhssin6rst@gmail.com>
2024-08-09 18:41:41 -0400

    add download from url test cases

--- tests/test_exceptions/import_exceptions.py
+++ tests/test_exceptions/import_exceptions.py
@@ -1,9 +1,9 @@
 # CORRUPTED_JSON_FILE = 1 -> tested.
 # INVALID_MODEL = 2 -> tested.
 # VALID_MODEL_INVALID_INTERNAL_STRUCTURE = 3 -> tested.
+import os
 from pymilo.pymilo_obj import Import
 
-import os
 
 def invalid_json(print_output = True):
     json_files = ["corrupted", "unknown-model"]
@@ -17,3 +17,14 @@ def invalid_json(print_output = True):
         if print_output: print("An Exception occured\n", e)
         return True
  
+def invalid_url():
+  url = "https://invalid_url"
+  Import(url=url)
+
+def valid_url_invalid_file():
+  url = "https://filesamples.com/samples/code/json/sample1.json"
+  Import(url=url)
+
+def valid_url_valid_file():
+  url = "https://raw.githubusercontent.com/openscilab/pymilo/main/tests/test_exceptions/valid_jsons/linear_regression.json"
+  Import(url=url)
