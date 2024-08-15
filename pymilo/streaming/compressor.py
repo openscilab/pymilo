3f463f02b992a1774c0f6548ac2bd6e23ba320b1
AHReccese <amirhssin6rst@gmail.com>
2024-08-15 12:18:39 -0400

    docstring for `DummyCompressor` class added

--- pymilo/streaming/compressor.py
+++ pymilo/streaming/compressor.py
@@ -2,11 +2,20 @@ from .interfaces import Compressor
 
 
 class DummyCompressor(Compressor):
+    """
+    The Pymilo DummyCompressor class is a dummy implementation of the Compressor interface to act as a simple wire.
+    """
 
     @staticmethod
     def compress(payload):
+        """
+        Compress the given payload in a dummy way, simply just return it (no compression applied)
+        """
         return payload
 
     @staticmethod
     def extract(payload):
+        """
+        Extract the given payload in a dummy way, simply just return it (no Extraction applied)
+        """
         return payload
