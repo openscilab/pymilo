bcdc0de10fca835b5ac8343f1bd340e104b5dea9
AmirHosein Rostami <32750909+AHReccese@users.noreply.github.com>
2024-12-28 13:31:21 -0500

    Refactor logistic regression (#171)
    * replace regression test with classification test
    
    * refactor code to use classifcation test same as the other classification models
    
    * remove logistic regression specific dataset preparation
    
    * `CHANGELOG.md` updated
    
    * Update CHANGELOG.md

--- tests/test_linear_models/logistic/logistic_regression_cv.py
+++ tests/test_linear_models/logistic/logistic_regression_cv.py
@@ -1,12 +1,12 @@
 from sklearn.linear_model import LogisticRegressionCV
-from pymilo.utils.test_pymilo import pymilo_regression_test
-from pymilo.utils.data_exporter import prepare_logistic_regression_datasets
+from pymilo.utils.test_pymilo import pymilo_classification_test
+from pymilo.utils.data_exporter import prepare_simple_classification_datasets
 
 MODEL_NAME = "Logistic-Regression-CV"
 
 
 def logistic_regression_cv():
-    x_train, y_train, x_test, y_test = prepare_logistic_regression_datasets()
+    x_train, y_train, x_test, y_test = prepare_simple_classification_datasets()
     # Create Logistic regression cv object
     logistic_regression_cv = 5
     logistic_regression_random_state = 0
@@ -15,5 +15,5 @@ def logistic_regression_cv():
         random_state=logistic_regression_random_state)
     # Train the model using the training sets
     logistic_regression_cv.fit(x_train, y_train)
-    assert pymilo_regression_test(
+    assert pymilo_classification_test(
         logistic_regression_cv, MODEL_NAME, (x_test, y_test)) == True
