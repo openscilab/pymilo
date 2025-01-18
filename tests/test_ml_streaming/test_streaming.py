39d4feb9764d06c6b037a9415b312baf5e3564c4
AHReccese <amirhssin6rst@gmail.com>
2025-01-17 19:35:42 -0500

    add `generate_dockerfile` testcases

--- tests/test_ml_streaming/test_streaming.py
+++ tests/test_ml_streaming/test_streaming.py
@@ -2,11 +2,12 @@ import os
 import time
 import pytest
 import subprocess
+from filecmp import cmp
 from sys import executable
 from scenarios.scenario1 import scenario1
 from scenarios.scenario2 import scenario2
 from scenarios.scenario3 import scenario3
-
+from pymilo.streaming.util import generate_dockerfile
 
 @pytest.fixture(
     scope="session",
@@ -72,3 +73,30 @@ def test2(prepare_bare_server):
 def test3(prepare_ml_server):
     _, compression_method, communication_protocol = prepare_ml_server
     assert scenario3(compression_method, communication_protocol) == 0
+
+
+def test_dockerfile():
+    docker_files_folder = os.path.join(
+        os.getcwd(),
+        "tests",
+        "test_ml_streaming",
+        "docker_files",
+    )
+    generate_dockerfile(
+        dockerfile_name="Dockerfile1",
+        model_path="https://raw.githubusercontent.com/openscilab/pymilo/main/tests/test_exceptions/valid_jsons/linear_regression.json")
+    r1 = cmp('Dockerfile1', os.path.join(
+        docker_files_folder,
+        "Dockerfile1"
+        )
+    )
+    generate_dockerfile(
+        dockerfile_name="Dockerfile2",
+        model_path="linear_regression.json",
+        )
+    r2 = cmp('Dockerfile2', os.path.join(
+        docker_files_folder,
+        "Dockerfile2"
+        )
+    )
+    assert r1 and r2
