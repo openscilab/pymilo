c73d2c728efef351e4ee54d72bf0274c410db485
AmirHosein Rostami <32750909+AHReccese@users.noreply.github.com>
2025-01-20 01:45:23 -0500

    Add/dockerfile (#182)
    * implement `generate_dockerfile` function
    
    * `CHANGELOG.md` updated
    
    * fix docstring issue
    
    * add ability to set `Dockerfile` name
    
    * add static Dockerfiles for `generate_dockerfile` testcase
    
    * add `generate_dockerfile` testcases
    
    * `CHANGELOG.md` updated
    
    * remove generated Dockerfiles after testcase execution
    
    * remove `\n` from the beginning of the line and add `\n` at the end of the file
    
    * update docstring
    
    * apply `autopep8.sh`

--- pymilo/streaming/util.py
+++ pymilo/streaming/util.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """ML Streaming utility module."""
+import os
 import re
+from ..pymilo_param import URL_REGEX
 
 
 def validate_websocket_url(url: str) -> str:
@@ -39,3 +41,71 @@ def validate_http_url(url: str) -> str:
     if not re.match(full_pattern, url):
         return False, None
     return True, url
+
+
+def generate_dockerfile(
+        dockerfile_name="Dockerfile",
+        model_path=None,
+        compression='NULL',
+        protocol='REST',
+        port=8000,
+        init_model=None,
+        bare=False
+):
+    """
+    Generate a Dockerfile for running a PyMilo server with specified configurations.
+
+    :param dockerfile_name: Name of the dockerfile.
+    :type dockerfile_name: str
+    :param model_path: Path or URL to the exported model JSON file.
+    :type model_path: str
+    :param compression: Compression method (default: NULL).
+    :type compression: str
+    :param protocol: Communication protocol (default: REST).
+    :type protocol: str
+    :param port: Port for the PyMilo server (default: 8000).
+    :type port: int
+    :param init_model: The model that the server initialized with.
+    :type init_model: boolean
+    :param bare: A flag that sets if the server runs without an internal ML model.
+    :type bare: boolean
+    """
+    dockerfile_content = f"""# Use an official Python runtime as a parent image
+FROM python:3.11-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Install pymilo
+RUN pip install pymilo[streaming]
+    """
+    is_url = False
+    if model_path:
+        if re.match(URL_REGEX, model_path):
+            is_url = True
+        else:
+            dockerfile_content += f"\nCOPY {os.path.basename(model_path)} /app/model.json"
+
+    # Expose the specified port
+    dockerfile_content += f"\nEXPOSE {port}"
+
+    cmd = "CMD [\"python\", \"-m\", \"pymilo\""
+    cmd += f", \"--compression\", \"{compression}\""
+    cmd += f", \"--protocol\", \"{protocol}\""
+    cmd += f", \"--port\", \"{port}\""
+
+    if model_path:
+        if is_url:
+            cmd += f", \"--load\", \"{model_path}\""
+        else:
+            cmd += f", \"--load\", \"/app/model.json\""
+    elif init_model:
+        cmd += f", \"--init\" \"{init_model}\""
+    elif bare:
+        cmd += f", \"--bare\""
+
+    cmd += "]"
+    dockerfile_content += f"\n{cmd}\n"
+
+    with open(dockerfile_name, 'w') as f:
+        f.write(dockerfile_content)
--- tests/test_ml_streaming/test_streaming.py
+++ tests/test_ml_streaming/test_streaming.py
@@ -2,11 +2,12 @@ import os
 import time
 import pytest
 import subprocess
+from filecmp import cmp
 from sys import executable
 from scenarios.scenario1 import scenario1
 from scenarios.scenario2 import scenario2
 from scenarios.scenario3 import scenario3
-
+from pymilo.streaming.util import generate_dockerfile
 
 @pytest.fixture(
     scope="session",
@@ -94,3 +95,31 @@ def test2(prepare_bare_server):
 def test3(prepare_ml_server):
     compression_method, communication_protocol = prepare_ml_server
     assert scenario3(compression_method, communication_protocol) == 0
+
+
+def test_dockerfile():
+    docker_files_folder = os.path.join(
+        os.getcwd(),
+        "tests",
+        "test_ml_streaming",
+        "docker_files",
+    )
+    generate_dockerfile(
+        dockerfile_name="Dockerfile",
+        model_path="https://raw.githubusercontent.com/openscilab/pymilo/main/tests/test_exceptions/valid_jsons/linear_regression.json")
+    r1 = cmp('Dockerfile', os.path.join(
+        docker_files_folder,
+        "Dockerfile1"
+        )
+    )
+    generate_dockerfile(
+        dockerfile_name="Dockerfile",
+        model_path="linear_regression.json",
+        )
+    r2 = cmp('Dockerfile', os.path.join(
+        docker_files_folder,
+        "Dockerfile2"
+        )
+    )
+    os.remove(path='Dockerfile')
+    assert r1 and r2
