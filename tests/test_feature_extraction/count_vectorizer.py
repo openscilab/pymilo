b541ec282af7c2d1e9543d77a7d4ab5ffc4ae6bb
AHReccese <amirhssin6rst@gmail.com>
2025-02-14 18:42:21 -0500

    `CountVectorizer` testcase

---
+++ tests/test_feature_extraction/count_vectorizer.py
@@ -0,0 +1,34 @@
+from numpy import array_equal, random
+from sklearn.datasets import load_sample_images
+from sklearn.feature_extraction.text import CountVectorizer
+from pymilo.transporters.feature_extraction_transporter import FeatureExtractorTransporter
+from pymilo.utils.test_pymilo import report_status
+from util import get_path, write_and_read
+
+MODEL_NAME = "CountVectorizer"
+
+def count_vectorizer():
+    corpus = [
+        'This is the first document.',
+        'This document is the second document.',
+        'And this is the third one.',
+        'Is this the first document?',
+    ]
+    cv = CountVectorizer(analyzer='word', ngram_range=(2, 2))
+    X = cv.fit_transform(corpus)
+    cv.get_feature_names_out()
+
+    pre_result = X.toarray()
+
+    fe = FeatureExtractorTransporter()
+    print("before: \n",cv.__dict__)
+    post_pymilo_pre_model = fe.deserialize_fe_module(
+        write_and_read(
+            fe.serialize_fe_module(cv),
+            get_path(MODEL_NAME)))
+    print("before: \n",post_pymilo_pre_model.__dict__)
+    post_result = post_pymilo_pre_model.fit_transform(corpus).toarray()
+
+    comparison_result = array_equal(pre_result, post_result)
+    report_status(comparison_result, MODEL_NAME)
+    assert comparison_result
