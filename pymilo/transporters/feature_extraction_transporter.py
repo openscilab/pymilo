a81595263683a3b673503ea3bb6417723fb3163f
AHReccese <amirhssin6rst@gmail.com>
2025-02-15 12:36:19 -0500

    init with shape

--- pymilo/transporters/feature_extraction_transporter.py
+++ pymilo/transporters/feature_extraction_transporter.py
@@ -119,7 +119,7 @@ class FeatureExtractorTransporter(AbstractTransporter):
                 data[key] = self.deserialize_fe_module(data[key])
             elif check_str_in_iterable("pymilo-csr_matrix", data[key]):
                 csr_matrix_dict = FEATURE_EXTRACTION_CHAIN["GeneralDataStructureTransporter"].get_deserialized_dict(data[key]["pymilo-csr_matrix"])
-                cm = csr_matrix()
+                cm = csr_matrix(csr_matrix_dict['_shape'])
                 for key in csr_matrix_dict:
                     setattr(cm, key, csr_matrix_dict[key])
                 data[key] = cm
