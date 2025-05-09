6d3e8806abee58044c43c3997b686dd56337e820
AHReccese <amirhssin6rst@gmail.com>
2025-05-09 11:56:04 -0400

    update python version to 3.7

--- setup.py
+++ setup.py
@@ -51,13 +51,12 @@ setup(
     extras_require={
         'streaming': get_requires(mode='streaming'),
     },
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
