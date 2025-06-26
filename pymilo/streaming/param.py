8a7210835c43d8badad0d801aed9fd03cce734e9
AHReccese <amirhssin6rst@gmail.com>
2025-06-26 12:47:04 -0400

    add api version param

--- pymilo/streaming/param.py
+++ pymilo/streaming/param.py
@@ -10,3 +10,5 @@ PYMILO_CLIENT_FAILED_TO_DOWNLOAD_REMOTE_MODEL = "PyMiloClient failed to download
 PYMILO_SERVER_NON_EXISTENT_ATTRIBUTE = "The requested attribute doesn't exist in this model."
 PYMILO_INVALID_URL = "The given URL is not valid."
 PYMILO_CLIENT_WEBSOCKET_NOT_CONNECTED = "WebSocket is not connected."
+
+REST_API_PREFIX = "/api/v1"
